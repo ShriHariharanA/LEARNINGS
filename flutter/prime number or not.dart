f8ab07fd31d831e297be91dc80001471df20d4d9
ShriHariharanA <79576542+ShriHariharanA@users.noreply.github.com>
2021-02-26 15:13:12 +0530

    Add files via upload

---
+++ flutter/prime number or not.dart
@@ -0,0 +1,15 @@
+void main(){
+var i=1, count=0, num=22;
+for(i=0;i<=num; i++){ 
+  if (i<=num){
+    if(num%i==0){
+      count+=1;
+    }     
+    }
+  }
+  if(count==2)
+    print("the given number is Prime");
+  else
+    print("given number is not a prime");
+    
+}
